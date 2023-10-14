fc957ec0c8244c6a1475fbecbd78b6008d8639c3
samzyconcepts <samson.oderinwale@kibo.school>
2023-10-14 01:13:06 +0100

    add range of number and fix render template

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/fizzbuzz")
-def fizzbuzz():
+def fizzbuzz_display():
     numbers = range(1, 101)
-    return render_template('fizzbuzz.html', numbers=numbers)
+    return render_template("fizzbuzz.html", numbers=numbers)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
